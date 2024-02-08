5886653ffe82ebc6685efd26cbfd2b2fec0c8e13
Qendrim Behrami <behrami@in.tum.de>
2024-02-09 00:50:43 +0100

    Add dimension changer
    Now it is possible to dynamically change the
    dimension of the sketchPad. Changing the dimension
    deletes the old content and creates a blank
    sketchPad

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-let sketchPadSize = 16;
 let hoverColor = "#ff757f";
+const MAX_DIMENSION = 100;
+const MIN_DIMENSION = 1;
 
-function createSketchPad(sketchPadSize) {
+function createSketchPad(sketchPadDimension) {
   let sketchPadContainer = document.querySelector("#sketchPadContainer");
+  sketchPadContainer.replaceChildren();
 
   // Get size of whole sketchPad
   let sketchPadWidth = sketchPadContainer.clientWidth;
   let sketchPadHeight = sketchPadContainer.clientHeight;
 
   // Compute size of each sketchPad element
-  let sketchPadElementWidth = sketchPadWidth / sketchPadSize;
-  let sketchPadElementHeight = sketchPadHeight / sketchPadSize;
+  let sketchPadElementWidth = sketchPadWidth / sketchPadDimension;
+  let sketchPadElementHeight = sketchPadHeight / sketchPadDimension;
 
-  for (let i = 0; i < sketchPadSize; i++) {
+  for (let i = 0; i < sketchPadDimension; i++) {
     // Add one row element to sketchPad
     let sketchPadRow = document.createElement("div");
     sketchPadRow.classList.add("sketchPadRow");
@@ -20,12 +22,13 @@ function createSketchPad(sketchPadSize) {
     sketchPadContainer.appendChild(sketchPadRow);
 
     // Create the elements within the row
-    for (let j = 0; j < sketchPadSize; j++) {
+    for (let j = 0; j < sketchPadDimension; j++) {
       let sketchPadElement = document.createElement("div");
       sketchPadElement.classList.add("sketchPadElement");
       sketchPadElement.style.width = sketchPadElementWidth + "px";
       sketchPadElement.style.height = sketchPadElementHeight + "px";
 
+      // Setup the hover effect
       sketchPadElement.addEventListener("mouseenter", (event) => {
         sketchPadElement.style.backgroundColor = hoverColor;
       });
@@ -35,4 +38,18 @@ function createSketchPad(sketchPadSize) {
   }
 }
 
-createSketchPad(sketchPadSize);
+// Enable adaptive dimension
+let dimensionInputField = document.querySelector("#dimensionInputField");
+dimensionInputField.addEventListener("input", function () {
+  let dimension = dimensionInputField.value;
+  if (dimension >= MIN_DIMENSION && dimension <= MAX_DIMENSION) {
+    createSketchPad(dimensionInputField.value);
+  } else {
+    alert(
+      `Please provide a dimension between ${MIN_DIMENSION} and ${MAX_DIMENSION}`
+    );
+  }
+});
+
+// Initial SketchPad
+createSketchPad(dimensionInputField.value);
